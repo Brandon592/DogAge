fa1fba3269f59ed579d7aba34fcbf267f125ad3b
Brandon Holland <Brandon@Brandons-MacBook-Air-2.local>
2015-01-28 19:22:36 -0500

    Practice app

--- DogAge/ViewController.swift
+++ DogAge/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var enterYearsField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButton(sender: UIButton) {
+        if (enterYearsField.text == ""){
+        dogYearsLabel.text = "Dog Years: "
+        }
+        else{
+        dogYearsLabel.text = "Dog Years: \(enterYearsField.text.toInt()!*7)"
+        }
+        enterYearsField.resignFirstResponder()
+    }
 
+    @IBAction func realConvertButton(sender: UIButton) {
+        if (enterYearsField.text == ""){
+            dogYearsLabel.text = "Dog Years: "
+        }
+        else{
+            var age:Double
+            var y = Double(enterYearsField.text.toInt()!)
+            if (y <= 2){
+                age = y*10.5;
+            }else{
+                age = 21 + (y-2)*4
+            }
+            dogYearsLabel.text = "Dog Years: \(age)"
+        }
+        enterYearsField.resignFirstResponder()
+    }
 }
 
